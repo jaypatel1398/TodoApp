14966d63850c618d950a56fcfc8d348be571cb18
jaypatel1398 <jay13698patel@gmail.com>
2020-02-24 22:50:32 +0530

    TodoApp-redux-immutability-helper

--- src/reducers/TodoListReducer.js
+++ src/reducers/TodoListReducer.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import {actionTypes} from '../constants/actionTypes';
+import update from 'immutability-helper';
 
 const initialState = {
 	byId: {},
@@ -11,68 +12,77 @@ const todoListReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case (actionTypes.ADD_TODO):
 			const currentId = state.allIds.length===0 ? 1 : state.allIds[state.allIds.length-1] + 1;
-			return ({
-				...state,
+			return update(state, {
 				byId: {
-					...state.byId,
-					[currentId] : {
-						task: action.payload.task,
-						id: currentId,
-						completed: false
+					$merge: {
+						[currentId] : {
+							task: action.payload.task,
+							id: currentId,
+							completed: false
+						}
 					}
 				},
-				allIds: [
-					...state.allIds,
-					currentId
-				],
-				activeIds: [
-					...state.activeIds,
-					currentId
-				]
+				allIds: {
+					$push: [currentId]
+				},
+				activeIds: {
+					$push: [currentId]
+				}
 			})
 		case (actionTypes.TOGGLE_TODO):
-			const todo = state.byId[action.payload.id];
-			const todoIsActive = !todo.completed;
-			let tempActiveIds = [...state.activeIds];
-			let tempCompletedIds = [...state.completedIds];
-			const tempById = {...state.byId};
+			const todoIsActive = !state.byId[action.payload.id].completed;
 			if(todoIsActive){
-				const activeIndex = _.indexOf(tempActiveIds, action.payload.id);
-				tempActiveIds.splice(activeIndex, 1);
-				tempCompletedIds = [...tempCompletedIds, todo.id];
-				tempById[todo.id].completed = !todo.completed;
-				return({
-					byId: tempById,
-					allIds: state.allIds,
-					completedIds: tempCompletedIds,
-					activeIds: tempActiveIds
+				const activeIndex = _.indexOf(state.activeIds, action.payload.id);
+				return update(state, {
+					byId: {
+						[action.payload.id]:{
+							completed:{
+								$set: true
+							}
+						}
+					},
+					completedIds: {
+						$push: [action.payload.id]
+					},
+					activeIds: {
+						$splice: [[activeIndex,1]]
+					}
 				});
 			}
 			else{
-				const completedIndex = _.indexOf(tempCompletedIds, action.payload.id);
-				tempCompletedIds.splice(completedIndex, 1);
-				tempActiveIds = [...tempActiveIds, todo.id];
-				tempById[todo.id].completed = !todo.completed;
-				return({
-					byId: tempById,
-					allIds: state.allIds,
-					completedIds: tempCompletedIds,
-					activeIds: tempActiveIds
+				const completedIndex = _.indexOf(state.completedIds, action.payload.id);
+				return update(state, {
+					byId: {
+						[action.payload.id]:{
+							completed:{
+								$set: false
+							}
+						}
+					},
+					activeIds: {
+						$push: [action.payload.id]
+					},
+					completedIds: {
+						$splice: [[completedIndex, 1]]
+					}
 				});
 			}
 		case (actionTypes.CLEAR_COMPLETED):
-			let temprById = {...state.byId};
-			let temprAllIds = [...state.allIds];
-			_.forEach(state.completedIds, (id)=> {
-				delete temprById[id];
-				const index = _.indexOf(temprAllIds, id);
-				temprAllIds.splice(index, 1);
+			var tempAllIds = _.filter(state.allIds, (id) => {
+				if(state.byId[id].completed)
+					return false;
+				return true;
 			})
-			return({
-				allIds: temprAllIds,
-				activeIds: state.activeIds,
-				byId: temprById,
-				completedIds: []
+			return update(state, {
+				byId: {
+					$unset: state.completedIds
+				},
+				allIds: {
+					$set: tempAllIds
+				},
+				completedIds: {
+					$splice: [[0, state.completedIds.length]]
+				}
 			});
 		default:
 			return state;
